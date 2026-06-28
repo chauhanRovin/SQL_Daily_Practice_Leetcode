3ac9ce7cf8a5dfd5b4095df9debf197fad3ddd4c
ROVIN CHAUHAN <rovinchauhan.97@gmail.com>
2026-06-28 19:15:53 +0530

    Update Replace_Employee_ID_With_The_Unique_Identifier.sql

--- Day_03/Replace_Employee_ID_With_The_Unique_Identifier.sql
+++ Day_03/Replace_Employee_ID_With_The_Unique_Identifier.sql
@@ -1,1 +1,14 @@
+-- Problem: Product Sales Analysis I
+-- Difficulty: Easy
+-- Concepts: SELECT, FROM, LEFT JOIN, ON
 
+
+SELECT 
+    u.unique_id,
+    e.name
+FROM 
+    Employees e
+LEFT JOIN 
+    EmployeeUNI u
+ON 
+    e.id = u.id;
