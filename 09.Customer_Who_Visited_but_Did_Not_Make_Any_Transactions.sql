5b7f9cf846677c6bafc9ab54f329e1340aae7e73
ROVIN CHAUHAN <rovinchauhan.97@gmail.com>
2026-06-29 19:12:41 +0530

    Add query for customers with no transactions

---
+++ 09.Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
@@ -0,0 +1,7 @@
+SELECT v.customer_id,
+       COUNT(v.customer_id) AS count_no_trans
+FROM Visits as v
+LEFT JOIN Transactions as t
+    ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
