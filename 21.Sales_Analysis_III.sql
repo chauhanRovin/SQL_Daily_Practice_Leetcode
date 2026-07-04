84af5ddae37712dd7084000ddc935c819c691caf
ROVIN CHAUHAN <rovinchauhan.97@gmail.com>
2026-07-04 11:07:37 +0530

    Update SQL query for sales analysis with HAVING clause

---
+++ 21.Sales_Analysis_III.sql
@@ -0,0 +1,11 @@
+-- Problem: Sales Analysis III
+-- Difficulty: Easy
+-- Concepts: SELECT, FROM, JOIN, ON, HAVING, MIN, MAX
+
+SELECT p.product_id, 
+       p.product_name
+FROM Product p
+JOIN Sales s
+ON p.product_id = s.product_id
+GROUP BY s.product_id
+HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31'
